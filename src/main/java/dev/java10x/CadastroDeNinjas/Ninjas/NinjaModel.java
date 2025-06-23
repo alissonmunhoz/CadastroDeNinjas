d6dd1e4ce5ca99f63f8ae90e8fadf5c97b2c20ac
Alisson Munhoz <als.munhoz@gmail.com>
2025-06-23 01:51:44 -0300

    modelagem do banco de dados ninjaModel e missoesModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
@@ -10,7 +13,10 @@ public class NinjaModel {
     private Long id;
     private String nome;
     private String email;
-    int idade;
+    private int idade;
+    @ManyToOne
+    @JoinColumn(name  = "missoes_id")
+    private MissoesModel missoes;
 
 
     public NinjaModel(String nome, String email, int idade) {
