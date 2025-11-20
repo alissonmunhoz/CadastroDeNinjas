88a3f2499ef3a3df1d52086402ee267ecd8ec919
Alisson Munhoz <als.munhoz@gmail.com>
2025-11-20 18:26:32 -0300

    first commit

---
+++ src/main/java/dev/munhoz/CastrroDeNinjas/CastrroDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.munhoz.CastrroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastrroDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastrroDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/munhoz/CastrroDeNinjas/CastrroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.munhoz.CastrroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastrroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
